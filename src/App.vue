d58e51278777f5b8401bfd0581b0a7b27154ea21
Justin B. Watson <justin@brimelive.com>
2021-12-13 18:38:15 -0600

    #BrimeTime

--- src/App.vue
+++ src/App.vue
@@ -380,8 +380,8 @@
       <div class="bg-black">
         <div class="max-w-4xl mx-auto py-16 px-4 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 lg:flex lg:items-center lg:justify-between">
           <h2 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-            <span class="block">Ready to get started?</span>
-            <span class="block bg-gradient-to-r bg-gradient-to-r from-rose-500 to-pink-600 bg-clip-text text-transparent">Get in touch or create an account.</span>
+            <span class="block text-white">Get in touch or create an account</span>
+            <span class="block bg-gradient-to-r bg-gradient-to-r from-rose-500 to-pink-600 bg-clip-text text-transparent">#BrimeTime</span>
           </h2>
           <div class="mt-6 space-y-4 sm:space-y-0 sm:flex sm:space-x-5">
             <a href="#" class="flex items-center justify-center bg-gradient-to-r bg-gradient-to-r from-rose-500 to-pink-600 bg-origin-border px-4 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white hover:from-purple-700 hover:to-indigo-700">
@@ -556,7 +556,7 @@ const verticles = [
     icon: GlobeIcon,
   },
   { name: 'Original Content Owners', 
-    description: "Realtime chat technology that let's your viewers enjoy content together", 
+    description: "Have a catelog of content that you want to share with the world?", 
     href: '#', 
     icon: ChatAltIcon },
   {
